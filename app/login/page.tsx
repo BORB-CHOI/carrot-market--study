3706c0d130e7111813af175dc31e726dbdbf3ce6
BORB <qzsec299@naver.com>
2024-06-20 17:28:45 +0900

    useFormStatus

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,39 @@
 import FormBtn from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { useFormStatus } from "react-dom";
 
 export default function Login() {
+  const handleForm = async (formData: FormData) => {
+    "use server";
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    console.log("what the fuck");
+  };
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[""]} />
+      <form action={handleForm} className="flex flex-col gap-3">
+        <FormInput
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          errors={[""]}
+        />
         <FormInput
           type="password"
+          name="password"
           placeholder="Password"
           required
           errors={[""]}
         />
-        <FormBtn isLoading={false} text="Log in" />
+        <FormBtn text="Log in" />
       </form>
       <SocialLogin />
     </div>
--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,18 +1,21 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 interface FormBtnProps {
   text: string;
-  isLoading: boolean;
 }
 
-export default function FormBtn({ text, isLoading, ...props }: FormBtnProps) {
+export default function FormBtn({ text }: FormBtnProps) {
+  const { pending } = useFormStatus();
   return (
     <button
-      disabled={isLoading}
-      {...props}
+      disabled={pending}
       className={
         "primary-btn h-10 disabled:bg-neutral-400 disabled:cursor-not-allowed disabled:text-neutral-300"
       }
     >
-      {isLoading ? "로딩중..." : text}
+      {pending ? "로딩중..." : text}
     </button>
   );
 }
