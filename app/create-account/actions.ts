82e20f9d616f11bea9aacb11ce42c8152fa11d30
BORB <qzsec299@naver.com>
2024-06-22 17:34:50 +0900

    Refinement
    단일 오류 생성

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,50 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(20),
-  email: z.string().email(),
-  password: z.string().min(8),
-  confirm_password: z.string().min(8),
-});
+const checkUsername = (username: string) => {
+  return !username.includes("hi") ? true : false;
+};
+
+const checkPassword = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => {
+  return password === confirm_password ? true : false;
+};
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "사용자 이름은 문자 형식이어야 합니다.",
+        required_error: "사용자 이름은 필수입니다.",
+      })
+      .min(3, "너무 짧으세요.")
+      .max(20, "너무 기세요.")
+      .refine(checkUsername, "hi 금지"),
+    email: z
+      .string({
+        message: "이메일 형식은 문자열이어야 합니다.",
+      })
+      .email({
+        message: "이메일 형식이 올바르지 않습니다.",
+      }),
+    password: z
+      .string({
+        message: "비밀번호 형식은 문자열이어야 합니다.",
+      })
+      .min(8, "비밀번호가 너무 짧으세요."),
+    confirm_password: z
+      .string({ message: "비밀번호 형식은 문자열이어야 합니다." })
+      .min(8, "비밀번호가 너무 짧다니깐?"),
+  })
+  .refine(checkPassword, {
+    message: "비밀번호가 일치하지 않습니다.",
+    path: ["confirm_password", "password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -27,6 +65,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (result.success === false) {
     // console.log(result.error);
     // flatten을 쓰면 error를 더 쉽게 볼 수 있다.
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
