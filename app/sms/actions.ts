5afd46a937b4d3a78982d8cc35e118a86a3787ad
BORB <qzsec299@naver.com>
2024-06-27 20:00:02 +0900

    Coerce

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,10 +1,19 @@
 "use server";
 
-export async function smsVerifycation(prevState: any, formData: FormData) {
+import { z } from "zod";
+import validator from "validator";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
+// 유저가 input에 무언가를 입력하면 그건 자동으로 string으로 변환된다. coerce(강제).number()를 사용하면 string을 number로 변환할 수 있다.
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+export async function smsLogin(prevState: any, formData: FormData) {
   const data = {
     number: formData.get("number"),
     token: formData.get("token"),
   };
 
-  console.log(data);
+  console.log(typeof formData.get("token"));
+  console.log(typeof tokenSchema.parse(data.token));
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -3,10 +3,10 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { useFormState } from "react-dom";
-import { smsVerifycation } from "./actions";
+import { smsLogin } from "./actions";
 
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerifycation, null);
+  const [state, dispatch] = useFormState(smsLogin, null);
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -15,17 +15,14 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input
-          name="number"
-          type="number"
-          placeholder="Phone number"
-          required
-        />
+        <Input name="phone" type="text" placeholder="Phone number" required />
         <Input
           name="token"
           type="number"
           placeholder="인증 번호 입력"
           required
+          min={100000}
+          max={999999}
         />
         <Button text="Verify" />
       </form>
