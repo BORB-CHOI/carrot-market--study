3ed00699ed8ad7826020cc21b605c88fc32a4368
BORB <qzsec299@naver.com>
2024-06-27 23:34:27 +0900

    SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,18 +2,42 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "잘못된 번호 형식입니다."
+  );
 
 // 유저가 input에 무언가를 입력하면 그건 자동으로 string으로 변환된다. coerce(강제).number()를 사용하면 string을 number로 변환할 수 있다.
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
   const data = {
-    number: formData.get("number"),
+    phone: formData.get("phone"),
     token: formData.get("token"),
   };
 
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(data.token));
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(data.phone);
+    if (!result.success) {
+      return { token: false, error: result.error.flatten() };
+    } else {
+      return { token: true };
+    }
+  } else {
+    const result = tokenSchema.safeParse(data.token);
+    if (!result.success) {
+      return { token: true, error: result.error.flatten() };
+    } else {
+      redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import Input from "@/components/input";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./actions";
 
+const initialState = {
+  token: false,
+  errors: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -15,16 +20,27 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="text" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="인증 번호 입력"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="Verify" />
+        {!state.token ? (
+          <Input
+            name="phone"
+            key="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <Input
+            name="token"
+            key="token"
+            type="number"
+            placeholder="인증 번호 입력"
+            required
+            min={100000}
+            max={999999}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
